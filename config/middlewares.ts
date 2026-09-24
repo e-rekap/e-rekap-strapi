a265ee83f2bd317fd957a5ba891b4cd79b4fe5d2
suryata <madeputra415@gmail.com>
2026-09-25 06:57:24 +0700

    feat: add Elasticsearch configuration and dependencies

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -11,6 +11,12 @@ const config: Core.Config.Middlewares = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'global::unauthenticated-401',
+    config: {
+      paths: ['^/api/my-job(/pending)?$', '^/api/my-job/[^/]+/(start|notes|complete|take-over)$'],
+    },
+  },
 ];
 
 export default config;

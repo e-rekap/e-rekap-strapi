8271fbf10b9943541c60842594708f26b275901f
suryata <madeputra415@gmail.com>
2026-09-25 09:45:33 +0700

    chore: remove unauthenticated-401 middleware and its configuration from the project

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -11,12 +11,6 @@ const config: Core.Config.Middlewares = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  {
-    name: 'global::unauthenticated-401',
-    config: {
-      paths: ['^/api/my-job(/pending)?$', '^/api/my-job/[^/]+/(start|notes|complete|take-over)$'],
-    },
-  },
 ];
 
 export default config;
